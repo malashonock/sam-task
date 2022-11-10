fa130a63b3c7410ca3ed20a0b5090b3b13a3fdf5
Yan Malashonak <malashonock@gmail.com>
2022-11-10 22:40:40 +0300

    feat: implement shopping cart clear on checkout

--- src/features/shoppingCart/components/Checkout/Checkout.tsx
+++ src/features/shoppingCart/components/Checkout/Checkout.tsx
@@ -1,14 +1,28 @@
 import ShoppingCartContext, {
   ShoppingCartState,
 } from 'features/shoppingCart/model/ShoppingCartContext';
+import { useState } from 'react';
 import { useContext } from 'react';
 import CartItems from '../CartItems/CartItems';
+import CheckoutToast from '../CheckoutToast/CheckoutToast';
 import ShoppingCart from '../ShoppingCart/ShoppingCart';
 
 export default function Checkout(): JSX.Element {
   const {
     shoppingCart: { itemsCount },
+    dispatchShoppingCartAction,
   } = useContext(ShoppingCartContext) as ShoppingCartState;
+
+  const [showToast, setShowToast] = useState(false);
+
+  const handleCheckout = (): void => {
+    setShowToast(true);
+  };
+
+  const clearShoppingCart = (): void => {
+    setShowToast(false);
+    dispatchShoppingCartAction({ type: 'clearItems' });
+  };
 
   return (
     <div className="checkout d-flex flex-column gap-2">
@@ -17,9 +31,14 @@ export default function Checkout(): JSX.Element {
         <ShoppingCart />
       </div>
       <CartItems />
-      <button className="checkout__btn btn btn-success mt-auto" disabled={itemsCount === 0}>
+      <button
+        className="checkout__btn btn btn-success mt-auto"
+        disabled={itemsCount === 0}
+        onClick={handleCheckout}
+      >
         Checkout
       </button>
+      <CheckoutToast show={showToast} onClose={clearShoppingCart} />
     </div>
   );
 }
